49f835db7aec8812aee59a6944f7045b8e5cedf9
eszterurban <90504676+eszterurban@users.noreply.github.com>
2021-11-09 22:38:02 +0100

    Add files via upload

---
+++ Ch08/use.cpp
@@ -0,0 +1,16 @@
+#include "my.cpp"
+#include <iostream>
+
+int foo;
+
+int main()
+{
+	foo = 7;
+	print_foo();
+	print(99);
+	char cc;
+	cout << "Enter any key to exit" << endl;
+	cin>>cc;
+	
+	return 0;
+}
---
+++ Ch08/use2.cpp
@@ -0,0 +1,47 @@
+#include "std_lib_facilities.h"
+
+void swap_v(int a, int b)
+{
+	int temp; 
+	temp = a; 
+	a = b;
+	b = temp;
+}
+
+void swap_r(int& a, int& b)
+{
+	int temp;
+	temp = a;
+	a = b;
+	b = temp;
+}	
+
+/*void swap_cr(const int& a, const int& b) Konstans paramétert próbálunk megváltoztatni
+{
+	int temp;
+	temp = a;
+	a = b;
+	b = temp;
+}*/		
+
+
+int main()
+{
+	int x = 7;
+	int y = 9;
+	swap_v(x,y);
+	swap_v(7,9);
+	cout << "The value of x is: " << x << endl << "The value of y is: " << y << endl;	
+	
+	const int cx = 7;
+	const int cy = 9;
+	//swap_r(cx,cy);
+	//swap_r(7.7,9.9); Double értékek adása
+	
+	double dx = 7.7;
+	double dy = 9.9;
+	//swap_cr(dx,dy);
+	//swap_cr(7.7,9.9);
+	
+	return 0;
+}
